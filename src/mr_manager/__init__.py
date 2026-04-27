436e89e95c26ac67a6a7aba2a535c1d081453995
Moritz Wieland <moritz.wieland@googlemail.com>
2026-04-27 11:59:09 +0200

    refactor: extract CLI helper functions

---
+++ src/mr_manager/__init__.py
@@ -0,0 +1,3 @@
+"""mr-manager package."""
+
+from mr_manager.cli.version_flag import get_installed_version as get_installed_version
---
+++ src/mr_manager/cli/__init__.py
@@ -0,0 +1,3 @@
+"""CLI support utilities for mr-manager."""
+
+from .version_flag import handle_version_flag as handle_version_flag
